973a81677a9f7d8b34e3e81fe08f72e29388dd27
Jelo <hed-fjnieves@smu.edu.ph>
2024-10-23 21:06:57 +0900

    Add files via upload
    mysql code

---
+++ loginAIS.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE loginDB;
+
+USE loginDB;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50),
+    password VARCHAR(255)
+);
+
+ALTER TABLE users
+ADD COLUMN login_attempts INT DEFAULT 0,
+ADD COLUMN lockout_until DATETIME DEFAULT NULL;
+
+ALTER TABLE users
+ADD COLUMN access_code VARCHAR(4);
+
+ALTER TABLE users ADD COLUMN role VARCHAR(10);
+
+ALTER TABLE users ADD COLUMN status VARCHAR(10) DEFAULT 'Pending';
+INSERT INTO users (username, password, role, status) VALUES ('admin', 'admin', 'admin','active');
+
+
+
